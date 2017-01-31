29999e2ab6425cb77e51e15af1590adc8a74eb89
almusto <ales.musto@gmail.com>
2017-01-31 09:19:35 -0500

    dict method lab

--- DictionaryMethodsLab.playground/Contents.swift
+++ DictionaryMethodsLab.playground/Contents.swift
@@ -14,21 +14,21 @@
 // write your code here
 
 
-
+var starWarsHeroes = ["Luke Skuwalker", "Princess Leia", "Han Solo", "Rey"]
 
 /*: question2
  ### 2. Now create another variable `Array` called `starWarsVillains` that contains the villains of _Star Wars_: Darth Vader and Emperor Palpatine.
  */
 // write your code here
 
-
+var starWarsVillains = ["Darth Vader", "Emperor Palpatine"]
 
 
 /*: question3
  ### 3. Finally, create a third variable `Array` called `starWarsDroids` that contains the droids of _Star Wars_: R2-D2, C-3P0, IG-88, and BB-8 (the cutest droid of them all).
  */
 // write your code here
-
+var starWarsDriods = ["R2-D2", "C-3PO","IG-88", "BB-8"]
 
 
 
@@ -36,7 +36,7 @@
  ### 4. Darn! We forgot to add Kylo Ren, the newest _Star Wars_ villain. Add "Kylo Ren" to `starWarsVillains` using the `append()` method.
  */
 // write your code here
-
+starWarsVillains.append("Kylo Ren")
 
 
 
@@ -44,6 +44,7 @@
  ### 5. On second thought, we don't really care that much about IG-88. Remove him from the `starWarsDroids` array using the `removeAtIndex()` method. (What index is "IG-88" at?)
  */
 // write your code here
+starWarsDriods.remove(at: 2)
 
 
 
@@ -52,7 +53,10 @@
  ### 6. Excellent! We want to store these arrays in a dictionary. Create a _variable_ dictionary called `starWarsCharacters` with the following keys: "Heroes", "Villains", and "Droids". Assign the appropriate variables that you created in the previous questions to these keys when you initialize the dictionary.
  */
 // write your code here
-
+var starWarsCharacters = ["Heros" : starWarsHeroes,
+                          "Villains" : starWarsVillains,
+                          "Driods" : starWarsDriods
+]
 
 
 
@@ -60,7 +64,7 @@
  ### 7. We forgot some characters! There are a lot of ne'er-do-wells in the _Star Wars_ universe, too. Create a variable array called `starWarsGangsters` with the names of these two shady characters: Watto, Jabba the Hutt.
  */
 // write your code here
-
+var starWarsGangsters = ["Wato", "Jabba the Hutt"]
 
 
 
@@ -69,14 +73,18 @@
  */
 // write your code here
 
-
+starWarsCharacters.updateValue(starWarsGangsters, forKey: "Gangsters")
 
 
 /*: question9
  ### 9. Use the dictionary's `keys` property to print out all the keys in your dictionary to the console, one line at a time (hint: use iteration). Do you see all four of the keys you created above?
  */
 // write your code here
+let starWarsCharKeys = Array(starWarsCharacters.keys)
 
+for char in starWarsCharacters.keys {
+  print(char)
+}
 
 
 
@@ -85,14 +93,18 @@
  */
 // write your code here
 
-
+print(starWarsCharacters.count)
 
 
 /*: question11
  ### 11. Use an if statement and the dictionary's `isEmpty` property to print "The dictionary is empty!" to the console if it is empty; otherwise, print "There are <Count> items in the dictionary." What do you expect to see in the console? What _do_ you see?
  */
 // write your code here
-
+if starWarsCharacters.isEmpty {
+  print("The dictionary is empty!")
+} else {
+  print("There are \(starWarsCharacters.count) items in the dictionary.")
+}
 
 
 
@@ -100,7 +112,7 @@
  ### 12. We forgot some more pretty important characters. Create another variable array called `starWarsJedi`. Add the names of these important _Star Wars_ Jedi: Yoda, Obi-Wan Kenobi, Mace Windu.
  */
 // write your code here
-
+var starWarsJedi = ["Yoda", "Obi-Wan Kenobi", "Mace", "Windu"]
 
 
 
@@ -109,6 +121,12 @@
  */
 // write your code here
 
+if starWarsCharacters.updateValue(starWarsJedi, forKey: "Jedi") == nil {
+  print("Added Jedi Key")
+} else {
+  print("Updated Jedi Key")
+}
+
 
 
 
@@ -116,7 +134,11 @@
  ### 14. I can't remember if we added the a key/value pair for "Bounty Hunters" or not. Regardless, let's not worry about them for now. Use the `removeValueForKey()` method to remove "Bounty Hunters" from `starWarsCharacters`. Print the message "Removed Bounty Hunters" if the key was present in the dictionary, or "Bount Hunters did not exist" if it wasn't.
  */
 // write your code here
-
+if starWarsCharacters.removeValue(forKey: "Bounty Hunters") == nil {
+  print("Bounty Hunters did not exist")
+} else {
+  print("Removed Bounty Hunters")
+}
 
 
 
@@ -126,12 +148,16 @@
  After you've fixed the error, print out the contents of the "Jedi" key in `starWarsCharacters` to make sure your code works.
  */
 
-/* Start by uncommenting this block of code
+//Start by uncommenting this block of code
+
  if var jedi = starWarsCharacters["Jedi"] {
  jedi.append("Aayla Secura")
+  starWarsCharacters["Jedi"] = jedi
  }
- */
 
+if let jedis = starWarsCharacters["Jedi"] {
+  print(jedis)
+}
 
 
 
@@ -139,13 +165,13 @@
  ### 16. We want to print out the names of our _Star Wars_ heroes line-by-line. Unfortunately, the code below isn't working correctly -- nothing gets printed! Can you fix it so that our heroes are printed out? (Again, the code has been commented out so that it doesn't interfere with earlier questions until they have been completed. Start by uncommenting the code, but there's still another bug!)
  */
 
-/*
+
  if let heroes = starWarsCharacters["Heros"] {
  for hero in heroes {
  print(hero)
  }
  }
- */
+
 
 
 
